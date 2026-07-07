01d821c554a543ae8d7718a149ba894859eddc28
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2026-07-07 22:46:19 +0530

    Add error handling for reading config * test errors * add testdata for tests

--- config.go
+++ config.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"os"
 
 	"github.com/spf13/viper"
 )
 
-var ErrInvalidConfig = errors.New("Invalid config")
+var (
+	ErrInvalidConfig    = errors.New("invalid config")
+	ErrConfigNotFound   = errors.New("config file not found")
+	ErrPermissionDenied = errors.New("premission defined reading config file")
+	ErrMissingFields    = errors.New("validation error: 'filepath' and 'linkpath' are required for all links")
+)
 
 type Link struct {
 	Filepath string `mapstructure:"filepath"`
@@ -20,11 +27,22 @@ type Config struct {
 func readConfig(path string) (*Config, error) {
 	viper.SetConfigFile(path)
 	if err := viper.ReadInConfig(); err != nil {
-		return nil, ErrInvalidConfig
+		if errors.Is(err, os.ErrNotExist) {
+			return nil, ErrConfigNotFound
+		}
+		if errors.Is(err, os.ErrPermission) {
+			return nil, ErrPermissionDenied
+		}
+		return nil, fmt.Errorf("%w: %v", ErrInvalidConfig, err)
 	}
 	var cfg Config
 	if err := viper.Unmarshal(&cfg); err != nil {
-		return nil, ErrInvalidConfig
+		return nil, fmt.Errorf("%w: %v", ErrInvalidConfig, err)
+	}
+	for _, links := range cfg.Links {
+		if links.Filepath == "" || links.Linkpath == "" {
+			return nil, ErrMissingFields
+		}
 	}
 	return &cfg, nil
 }
--- config_test.go
+++ config_test.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 var (
@@ -12,62 +16,65 @@ var (
 	tomllinkpath  = "./links/toml/"
 )
 
-func TestReadConfigWithJSON(t *testing.T) {
+func TestReadConfig_JSON(t *testing.T) {
 	path := "./testdata/test.json"
 	cfg, err := readConfig(path)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if cfg.Links[0].Filepath != test1filepath {
-		t.Errorf("expected: filepath: %v, got: filepath: %v", test1filepath, cfg.Links[0].Filepath)
-	}
-	if cfg.Links[1].Filepath != test2filepath {
-		t.Errorf("expected: filepath: %v, got: filepath: %v", test2filepath, cfg.Links[1].Filepath)
-	}
-	if cfg.Links[0].Linkpath != jsonlinkpath {
-		t.Errorf("expected: linkpath: %v, got: linkpath: %v", jsonlinkpath, cfg.Links[0].Linkpath)
-	}
-	if cfg.Links[1].Linkpath != jsonlinkpath {
-		t.Errorf("expected: linkpath: %v, got: linkpath: %v", jsonlinkpath, cfg.Links[1].Linkpath)
-	}
+	assert.Equal(t, cfg.Links[0].Filepath, test1filepath)
+	assert.Equal(t, cfg.Links[1].Filepath, test2filepath)
+	assert.Equal(t, cfg.Links[0].Linkpath, jsonlinkpath)
+	assert.Equal(t, cfg.Links[1].Linkpath, jsonlinkpath)
 }
 
-func TestReadConfigWithYAML(t *testing.T) {
+func TestReadConfig_YAML(t *testing.T) {
 	path := "./testdata/test.yaml"
 	cfg, err := readConfig(path)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if cfg.Links[0].Filepath != test1filepath {
-		t.Errorf("expected: filepath: %v, got: filepath: %v", test1filepath, cfg.Links[0].Filepath)
-	}
-	if cfg.Links[1].Filepath != test2filepath {
-		t.Errorf("expected: filepath: %v, got: filepath: %v", test2filepath, cfg.Links[1].Filepath)
-	}
-	if cfg.Links[0].Linkpath != yamllinkpath {
-		t.Errorf("expected: linkpath: %v, got: linkpath: %v", yamllinkpath, cfg.Links[0].Linkpath)
-	}
-	if cfg.Links[1].Linkpath != yamllinkpath {
-		t.Errorf("expected: linkpath: %v, got: linkpath: %v", yamllinkpath, cfg.Links[1].Linkpath)
-	}
+	assert.Equal(t, cfg.Links[0].Filepath, test1filepath)
+	assert.Equal(t, cfg.Links[1].Filepath, test2filepath)
+	assert.Equal(t, cfg.Links[0].Linkpath, yamllinkpath)
+	assert.Equal(t, cfg.Links[1].Linkpath, yamllinkpath)
 }
 
-func TestReadConfigWithTOML(t *testing.T) {
+func TestReadConfig_TOML(t *testing.T) {
 	path := "./testdata/test.toml"
 	cfg, err := readConfig(path)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if cfg.Links[0].Filepath != test1filepath {
-		t.Errorf("expected: filepath: %v, got: filepath: %v", test1filepath, cfg.Links[0].Filepath)
-	}
-	if cfg.Links[1].Filepath != test2filepath {
-		t.Errorf("expected: filepath: %v, got: filepath: %v", test2filepath, cfg.Links[1].Filepath)
-	}
-	if cfg.Links[0].Linkpath != tomllinkpath {
-		t.Errorf("expected: linkpath: %v, got: linkpath: %v", tomllinkpath, cfg.Links[0].Linkpath)
-	}
-	if cfg.Links[1].Linkpath != tomllinkpath {
-		t.Errorf("expected: linkpath: %v, got: linkpath: %v", tomllinkpath, cfg.Links[1].Linkpath)
-	}
+	assert.Equal(t, cfg.Links[0].Filepath, test1filepath)
+	assert.Equal(t, cfg.Links[1].Filepath, test2filepath)
+	assert.Equal(t, cfg.Links[0].Linkpath, tomllinkpath)
+	assert.Equal(t, cfg.Links[1].Linkpath, tomllinkpath)
+}
+
+func TestReadConfig_ErrConfigNotFound(t *testing.T) {
+	path := "./testdata/config.json"
+	_, err := readConfig(path)
+	assert.ErrorIs(t, err, ErrConfigNotFound)
+}
+
+func TestReadConfig_ErrConfigPremissionDenied(t *testing.T) {
+	tmpDir := os.TempDir()
+	defer os.RemoveAll(tmpDir)
+	path := filepath.Join(tmpDir, "config.toml")
+	_ = os.WriteFile(path, []byte(""), 0000)
+	_, err := readConfig(path)
+	assert.ErrorIs(t, err, ErrPermissionDenied)
+}
+
+func TestReadConfig_ErrInvalidConfig(t *testing.T) {
+	path := "./testdata/incorrect_config.yaml"
+	_, err := readConfig(path)
+	assert.ErrorIs(t, err, ErrInvalidConfig)
+}
+
+func TestReadConfig_ErrMissingFields(t *testing.T) {
+	path := "./testdata/incorrect_config.toml"
+	_, err := readConfig(path)
+	assert.ErrorIs(t, err, ErrMissingFields)
 }
